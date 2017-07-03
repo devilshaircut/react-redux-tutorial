57e32d7abe1940d4a98e348f4311d80d2f04bbec
Jason <devilshaircut@gmail.com>
2017-07-02 23:58:29 -0400

    Segment 1.0.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@
 // Module imports go here:
 // --------------------------------------------------
 
+import React from 'react'
+import { render } from 'react-dom'
+import App from './containers/app'
+
 
 // --------------------------------------------------
 // Initial React render() goes here:
 // --------------------------------------------------
 
+const target = document.querySelector('#root')
+
+render(
+  <App />,
+  target
+)
