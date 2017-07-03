63cb374c48459ac8b60dc7482558e6ea048662c4
Jason <devilshaircut@gmail.com>
2017-07-03 01:24:54 -0400

    Update navigation HTML template.

--- src/containers/reference/index.js
+++ src/containers/reference/index.js
@@ -1,46 +1,48 @@
-// <nav id="ns-navigation">
-//   <div id="ns-navigation-top-bar"> </div>
-//   <div id="ns-navigation-segments">
-//     <h4 className="ns-navigation-header">Segment Header One</h4>
-//     <ol className="ns-navigation-segment">
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Layers</span> <span className="ns-navigation-label" style="transition-delay: 0s;">Navigation Item 1</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Rows</span> <span className="ns-navigation-label" style="transition-delay: 0.1s;">Navigation Item 2</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Grid</span>&lt; span className="ns-navigation-label" style="transition-delay: 0.2s;"&gt;Navigation Item 3</span> <span className="ns-badge">50</span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Compose</span> <span className="ns-navigation-label" style="transition-delay: 0.3s;">Navigation Item 4</span> <span className="ns-badge">50</span> </span></li>
-//     </ol>
-//     <h4 className="ns-navigation-header">Segment Header Two</h4>
-//     <ol className="ns-navigation-segment">
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Picture</span> <span className="ns-navigation-label" style="transition-delay: 0.4s;">Navigation Item 5</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Sample</span> <span className="ns-navigation-label" style="transition-delay: 0.5s;">Navigation Item 6</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Font</span> <span className="ns-navigation-label" style="transition-delay: 0.6s;">Navigation Item 7</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">User</span> <span className="ns-navigation-label" style="transition-delay: 0.7s;">Navigation Item 8</span> <span className="ns-badge">50</span> </span></li>
-//     </ol>
-//     <h4 className="ns-navigation-header">Segment Header Three</h4>
-//     <ol className="ns-navigation-segment">
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Layers</span> <span className="ns-navigation-label" style="transition-delay: 0.8s;">Navigation Item 9</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Rows</span> <span className="ns-navigation-label" style="transition-delay: 0.9s;">Navigation Item 10</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Grid</span> <span className="ns-navigation-label" style="transition-delay: 1s;">Navigation Item 11</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Compose</span> <span className="ns-navigation-label" style="transition-delay: 1.1s;">Navigation Item 12</span> <span className="ns-badge">50</span> </span></li>
-//     </ol>
-//     <h4 className="ns-navigation-header">Segment Header Four</h4>
-//     <ol className="ns-navigation-segment">
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Picture</span> <span className="ns-navigation-label" style="transition-delay: 1.2s;">Navigation Item 13</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Sample</span> <span className="ns-navigation-label" style="transition-delay: 1.3s;">Navigation Item 14</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Font</span> <span className="ns-navigation-label" style="transition-delay: 1.4s;">Navigation Item 15</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">User</span> <span className="ns-navigation-label" style="transition-delay: 1.5s;">Navigation Item 16</span> <span className="ns-badge">50</span> </span></li>
-//     </ol>
-//     <h4 className="ns-navigation-header">Segment Header Five</h4>
-//     <ol className="ns-navigation-segment">
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Picture</span> <span className="ns-navigation-label" style="transition-delay: 1.6s;">Navigation Item 17</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Sample</span> <span className="ns-navigation-label" style="transition-delay: 1.7s;">Navigation Item 18</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">Font</span> <span className="ns-navigation-label" style="transition-delay: 1.8s;">Navigation Item 19</span> <span className="ns-badge">50</span> </span></li>
-//       <li className=""><span className="ns-navigation-segment-item"> <span className="ss-icon">User</span> <span className="ns-navigation-label" style="transition-delay: 1.9s;">Navigation Item 20</span> <span className="ns-badge">50</span> </span></li>
-//     </ol>
-//   </div>
-//   <div id="ns-navigation-submenu" className="">
-//     <h5 className="ns-navigation-submenu-header"> </h5>
-//     <ol>
-//       <li>Back To Main Menu</li>
-//     </ol>
-//   </div>
-// </nav>
+    // <nav id="ns-navigation">
+    //   <div id="ns-navigation-top-bar">
+    //     <div id="ns-hamburger-icon"></div>
+    //   </div>
+    //   <div id="ns-navigation-segments">
+    //     <h4 className="ns-navigation-header"><span>Segment Header One</span></h4>
+    //     <ol className="ns-navigation-segment">
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Layers</span> <span className="ns-navigation-label">Navigation Item 1</span> <span className="ns-badge">47</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Rows</span> <span className="ns-navigation-label">Navigation Item 2</span> <span className="ns-badge">38</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Grid</span> <span className="ns-navigation-label">Navigation Item 3</span> <span className="ns-badge">73</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Compose</span> <span className="ns-navigation-label">Navigation Item 4</span> <span className="ns-badge">61</span></span></li>
+    //     </ol>
+    //     <h4 className="ns-navigation-header"><span>Segment Header Two</span></h4>
+    //     <ol className="ns-navigation-segment">
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Picture</span> <span className="ns-navigation-label">Navigation Item 5</span> <span className="ns-badge">30</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Sample</span> <span className="ns-navigation-label">Navigation Item 6</span> <span className="ns-badge">88</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Font</span> <span className="ns-navigation-label">Navigation Item 7</span> <span className="ns-badge">67</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">User</span> <span className="ns-navigation-label">Navigation Item 8</span> <span className="ns-badge">12</span></span></li>
+    //     </ol>
+    //     <h4 className="ns-navigation-header"><span>Segment Header Three</span></h4>
+    //     <ol className="ns-navigation-segment">
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Layers</span> <span className="ns-navigation-label">Navigation Item 9</span> <span className="ns-badge">47</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Rows</span> <span className="ns-navigation-label">Navigation Item 10</span> <span className="ns-badge">38</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Grid</span> <span className="ns-navigation-label">Navigation Item 11</span> <span className="ns-badge">73</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Compose</span> <span className="ns-navigation-label">Navigation Item 12</span> <span className="ns-badge">61</span></span></li>
+    //     </ol>
+    //     <h4 className="ns-navigation-header"><span>Segment Header Four</span></h4>
+    //     <ol className="ns-navigation-segment">
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Picture</span> <span className="ns-navigation-label">Navigation Item 13</span> <span className="ns-badge">30</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Sample</span> <span className="ns-navigation-label">Navigation Item 14</span> <span className="ns-badge">88</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Font</span> <span className="ns-navigation-label">Navigation Item 15</span> <span className="ns-badge">67</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">User</span> <span className="ns-navigation-label">Navigation Item 16</span> <span className="ns-badge">12</span></span></li>
+    //     </ol>
+    //     <h4 className="ns-navigation-header"><span>Segment Header Five</span></h4>
+    //     <ol className="ns-navigation-segment">
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Picture</span> <span className="ns-navigation-label">Navigation Item 17</span> <span className="ns-badge">30</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Sample</span> <span className="ns-navigation-label">Navigation Item 18</span> <span className="ns-badge">88</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">Font</span> <span className="ns-navigation-label">Navigation Item 19</span> <span className="ns-badge">67</span></span></li>
+    //       <li><span className="ns-navigation-segment-item"><span className="ss-icon">User</span> <span className="ns-navigation-label">Navigation Item 20</span> <span className="ns-badge">12</span></span></li>
+    //     </ol>
+    //   </div>
+    //   <div id="ns-navigation-submenu">
+    //     <h5> </h5>
+    //     <ol>
+    //       <li>Back To Main Menu</li>
+    //     </ol>
+    //   </div>
+    // </nav>
