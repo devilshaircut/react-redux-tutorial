67844ea94ce2cc44c95d7f181c5c83499329b765
Jason <devilshaircut@gmail.com>
2017-07-03 00:01:42 -0400

    Add in CSS to master component.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './containers/app'
 
+import 'sanitize.css/sanitize.css'
+import './index.css'
+
 
 // --------------------------------------------------
 // Initial React render() goes here:
